70b411ad7e202cf9821d1f3bfa890eabc92f1728
smmartprogrammer <partabrajp@gmail.com>
2024-01-24 15:03:16 +0500

    string class01 completed

--- class01.py
+++ class01.py
@@ -1,10 +1,4 @@
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
-print("raghunathji")
+name:tuple[str, float, bool, int] = ("raghunathji", 700.5, True, 900)
+print(name) # print
+print(type(name)) # type
+print(id(name)) # physical address
