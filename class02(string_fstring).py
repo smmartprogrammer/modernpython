016087ef98d1792c51c08364b9cdccf8ba81775a
smmartprogrammer <partabrajp@gmail.com>
2024-01-31 15:04:10 +0500

    class02 fstring topic has been done

---
+++ class02(string_fstring).py
@@ -0,0 +1,9 @@
+# mypy examples
+# name: any = "Pakistan"
+# number: int = "7"
+
+# senior developers writing code style:
+name: str = "Partab Raj"
+father_name: str = "Laljee Chagan"
+print(name)
+print(father_name)
