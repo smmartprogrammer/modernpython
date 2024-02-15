075e18249ef664f6d143fd6b33f87ce8b6ac7ee5
smmartprogrammer <partabrajp@gmail.com>
2024-02-15 13:40:12 +0500

    class11 is in process, error handling

--- class11.py
+++ class11.py
@@ -1,1 +1,10 @@
-.
+a: list[str] =['a', 'b'] # mutable
+b: list[str] =['c', 'd'] # mutable
+
+print(id(a))
+print(id(b))
+
+print(2+5)
+print(a+b)
+print(a-b) # it should be plus sign, but mistakenly some developer has put minus, so it is not performing the 
+            # correct answer, so this sort of issues will going to be check through test handling, 
